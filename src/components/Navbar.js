b442eb7c6cef4e5725043474b4267b96fa2c10ff
Shreyashk-07 <khandagaleshreyash15@gmail.com>
2023-04-08 00:55:59 +0530

    navbaradded with responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,27 @@
+import { useRef } from "react";
 import React from "react";
 import "../styles/Navbar.css"
-
+import {FaBars,FaTimes} from "react-icons/fa"
 function Navbar() {
+  const navRef =useRef();
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
     return (
-      <nav>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </nav>
+      <header>
+        <h3>Notes ki Dunia</h3>
+        <nav className="nav-bar" ref={navRef}>
+          <a href="#">Notes </a>
+          <a href="#">About Us</a>
+          <a href="#">Sign In</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+           <FaTimes/>
+        </button>
+        </nav>
+        <button className="nav-btn" onClick={showNavBar}>
+          <FaBars/>
+        </button>
+      </header>
     );
   }
   
